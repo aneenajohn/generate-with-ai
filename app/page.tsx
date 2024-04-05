121d32e6de9c1d5c5772b71e453bd9398a67adea
Aneena John <aneenajohn@gmail.com>
2024-04-05 12:24:38 +0530

    Authentication for protected routes added with clerk auth package
    Signed-off-by: Aneena John <aneenajohn@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from '@/components/ui/button';
+import DashboardPage from '@/app/(dashboard)/dashboard/page';
 
 export default function Home() {
   return (
     <>
-      <p>Hellooo</p>
+      <p className='text-pretty font-bold'>Hellooo</p>
       <Button>Click me!</Button>
+
+      <hr />
+      <DashboardPage />
     </>
   );
 }
