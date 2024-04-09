29b47edc3b46ccdb9c4fef21b9a6cafd123e76c3
Aneena John <aneenajohn@gmail.com>
2024-04-09 19:49:10 +0530

    Add wip page for videos and music
    Signed-off-by: Aneena John <aneenajohn@gmail.com>

---
+++ app/(dashboard)/video/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Wip from '@/components/wip';
+
+const VideoPage = () => {
+  return (
+    <div className='w-full h-full flex place-items-center'>
+      <Wip />
+    </div>
+  );
+};
+
+export default VideoPage;
---
+++ components/wip.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import React from 'react';
+
+const WipPage = () => {
+  return (
+    <div>
+      <Image alt='work in progress' fill src='/wip.jpg' />
+    </div>
+  );
+};
+
+export default WipPage;
